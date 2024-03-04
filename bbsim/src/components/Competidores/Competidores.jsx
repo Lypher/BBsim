2497aff8ce19358db2cbc36605e60e511269e301
Lypher <acosta.enzo@hotmail.com>
2024-03-04 18:00:34 -0300

    interacciones

--- bbsim/src/components/Competidores/Competidores.jsx
+++ bbsim/src/components/Competidores/Competidores.jsx
@@ -6,6 +6,7 @@ function Competidores() {
   const [resultados, setResultados] = useState([]);
   const [mostrarResultado, setMostrarResultado] = useState(false);
   const [indiceMostrado, setIndiceMostrado] = useState(0);
+  const [eventos, setEventos] = useState([]);
 
   const handleNombreChange = (index, value) => {
     const nuevosNombres = [...nombres];
@@ -32,10 +33,39 @@ function Competidores() {
     setResultados(resultadosOrdenados.reverse());
     setMostrarResultado(true);
     setIndiceMostrado(0);
+    generarEventosAleatorios();
+  };
+
+  const generarEventosAleatorios = () => {
+    const eventosGenerados = [];
+
+    const tiposDeInteraccion = [
+      "se peleó con",
+      "se hizo amigo de",
+      "le gastó una broma a",
+      "tuvo una conversación intensa con",
+      "formó una alianza secreta con",
+    ];
+
+    for (let i = 0; i < 5; i++) {
+      const participanteA = Math.floor(Math.random() * nombres.length);
+      const participanteB = Math.floor(Math.random() * nombres.length);
+      const tipoInteraccion =
+        tiposDeInteraccion[
+          Math.floor(Math.random() * tiposDeInteraccion.length)
+        ];
+
+      eventosGenerados.push({
+        mensaje: `${nombres[participanteA]} ${tipoInteraccion} ${nombres[participanteB]}`,
+      });
+    }
+
+    setEventos(eventosGenerados);
   };
 
   const mostrarSiguienteResultado = () => {
     setIndiceMostrado((prevIndice) => prevIndice + 1);
+    generarEventosAleatorios(); // Generar nuevas interacciones al hacer clic en "Siguiente"
   };
 
   const handleSubmit = () => {
@@ -69,6 +99,13 @@ function Competidores() {
               Siguiente
             </button>
           )}
+
+          <h3>Acontecimientos:</h3>
+          <ul>
+            {eventos.map((evento, index) => (
+              <li key={index}>{evento.mensaje}</li>
+            ))}
+          </ul>
         </div>
       )}
     </div>
